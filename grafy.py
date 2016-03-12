af04f5c90be0e43cef435f9388a5c487e47aad21
sazon <ksazon@sigma.ug.edu.pl>
2016-03-12 22:10:18 +0100

    mam nadzieje, ze teraz bedzie ladnie
    Signed-off-by: sazon <ksazon@sigma.ug.edu.pl>

--- grafy.py
+++ grafy.py
@@ -2,26 +2,23 @@ import matplotlib.pyplot as plt
 import networkx as nx
 from string import ascii_uppercase
 
-global nodesNum
-nodesNum = int(raw_input("Podaj ilosc wierzcholkow: "))
-
-assert nodesNum > 1, "Podaj liczbe wieksza od 1"
-
 
 def degree(nodesPair):
-    if nodesPair == ['0', '0']:
-        degrees = []
-        print "Stopnie wierzcholkow to:"
-        for node in sorted(H.nodes()):
-            print "deg(", node, ")", " = ", H.node[node]
-            degrees.append(H.node[node])
-        print "Jest to graf ", max(degrees), "stopnia"
-        return H
     nodeName = nodesPair[0]
-    H.node[nodeName] += 1
+    G.node[nodeName] += 1
     nodeName = nodesPair[1]
-    H.node[nodeName] += 1
-    return H
+    G.node[nodeName] += 1
+    # return G
+
+
+def degreesPrint():
+    degrees = []
+    print "Stopnie wierzcholkow to:"
+    for node in sorted(G.nodes()):
+        print "deg(", node, ")", " = ", G.node[node]
+        degrees.append(G.node[node])
+    print "Jest to graf", max(degrees), "stopnia"
+    # return G
 
 
 def graphics():
@@ -37,33 +34,32 @@ def graphics():
     plt.savefig("graph.png")
     plt.show()
 
-letters = []
-G = nx.Graph()
-global H
 
+global nodesNum
+nodesNum = int(raw_input("Podaj ilosc wierzcholkow: "))
+assert nodesNum > 1, "Podaj liczbe wieksza od 1"
+
+letters = []
 for i in range(nodesNum):
     letter = ascii_uppercase[i]
     letters.append(letter)
 
+global G
+G = nx.Graph()
 G.add_nodes_from(letters)
-H = G.copy()
-for i in H.nodes():
-    H.node[i] = 0
+for i in G.nodes():
+    G.node[i] = 0
 
-print "Wierzchoki to: ", G.nodes()
-
-print "Podaj miedzy ktorymi wierzcholkami istnieje krawedz (po 1 wierzcholku)"
+print "Wierzchoki to: ", sorted(G.nodes())
+print "Podaj miedzy ktorymi wierzcholkami istnieje krawedz (np. ac)"
 print "Po wprowadzeniu wszystkich wprowadz 0, enter"
-nodesChar = "a"
+
 while True:
-    nodesPair = ['a', 'a']
-    nodesChar = raw_input("Miedzy wierzcholkiem: ")
-    if nodesChar == '0':
-        degree(['0', '0'])
+    nodesPair = raw_input(": ")
+    if nodesPair[0] == '0':
+        degreesPrint()
         break
-    nodesPair[0] = nodesChar.upper()
-    nodesChar = raw_input("oraz: ")
-    nodesPair[1] = nodesChar.upper()
+    nodesPair = nodesPair.upper()
     print nodesPair
     G.add_edge(*nodesPair)
     degree(nodesPair)
